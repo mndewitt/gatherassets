2ca0842cf50a3977a431fdd1d8be3712ce0a712f
mndewitt <mdewitt07@gmail.com>
2015-08-08 11:25:18 -0400

    abstract it into a class

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# gatherassets
-I had a bunch of legacy code that needed migrating.  The images all needed to move from one CDN to another, and the originals were long gone.  These code snippets will make copying the assets, whether the paths exist in css/less files, html, or in PHP code, a simple process.
+## gatherassets
+I had a bunch of legacy code that needed migrating.  The images all needed to move from one CDN to another, and the originals were long gone.  This class will find those remote URLS in css, less, sass, html, or php files and copy them to a local directory of your choice.  
+
+###Usage
+
+```php
+
+//Establish the class and declare the target directory in the constructor
+$gatherAssets = new gatherAssets('img/');
+
+//Copy any images contained in url() arguments in CSS, LESS, or SASS files
+$gatherAssets->fromCss('test.css');
+
+//Copy any images contained in the src attribute of <img> tags in html or php files
+$gatherAssets->fromHtml('test.html');
+
+//Easily clean the target URL directory if you made a mistake or want to start over
+$gatherAssets->cleanDir();
+
+```
--- gatherassets.php
+++ gatherassets.php
@@ -1,63 +1,91 @@
 <?php
 
-/*
-**  Extract assets from a less/css file
-*/
+class gatherAssets {
 
-//Credit to purcaholic: https://gist.github.com/purcaholic/1341358
+	private $imgDir = '';
 
-$css = file_get_contents('path/to/style.less');
+	/*
+	** Constructor.  Establishes the target image directory
+	** @param string imgDir
+	** @return void
+	*/
 
-preg_match_all('/url\(([\s])?([\"|\'])?(.*?)([\"|\'])?([\s])?\)/i', $css, $matches);
-// in case of found matches, the multi-dimensional array $matches contains following
-// important entries:
-// $matches[0]  (array)  List containing each string matching the full pattern, e. g. url("images/bg.gif")
-// $matches[3]  (array)  List containing matched url definitions, e. g. images/bg.gif
-if ($matches) {
-	foreach($matches[3] as $match) {
-		$id = substr($match, strrpos($match, '/') + 1);
-		$new_img_path = 'img/from-legacy-css/' . $id;
-		copy('http:' . $match, $new_img_path);
-		echo $match . " has been copied to " . $new_img_path . "<br/>";
+	function __construct($imgDir) {
+		//Do the user a favor and add a trailing slash if they did not add one
+		if(substr($imgDir, -1) !== '/') {
+			$imgDir .= '/';
+		}
+
+		$this->imgDir = $imgDir;
 	}
-}
 
-/*
-**  Extract assets from an associative array
-*/
+	/*
+	** Gathers background images from css/less/sass files
+	** @param string pathToFile
+	** @return void
+	*/
 
-$features = array(
-		array( 
-			"url" => "//cdn.mycdn.com/img/76155488/5d9b9ad887dc47b48ffabaaa47ee1ec3.png",
-			"title" => "My awesome file name",
-		),
-		array( 
-			"url" => "//cdn.mycdn.com/img/76155488/46eee5697edf48458296480f63ceb6b9.png",
-			"title" => "My awesome file name numero dos"
-		)
-	);
+	public function fromCss($pathToFile) {
+		//Credit to purcaholic: https://gist.github.com/purcaholic/1341358
 
-foreach($features as $f) {
-	$url = "http:" . $f["url"];
-	$title = strpreg_replace('/[ ,]+/', '', $f["title"]);
-	$new_img_path = '/img/' . $title . '.png';
-	copy($url, $new_img_path);
-	echo "copied " . $url . " to " . $new_img_path . "<br/>";
-}
+		$css = file_get_contents($pathToFile);
+		preg_match_all('/url\(([\s])?([\"|\'])?(.*?)([\"|\'])?([\s])?\)/i', $css, $matches);
 
-/*
-**  Extract assets from some html
-*/
+		// in case of found matches, the multi-dimensional array $matches contains following
+		// important entries:
+		// $matches[0]  (array)  List containing each string matching the full pattern, e. g. url("images/bg.gif")
+		// $matches[3]  (array)  List containing matched url definitions, e. g. images/bg.gif
 
-$html = file_get_contents('path/to/myfile.php');
+		if ($matches) {
+			foreach($matches[3] as $match) {
+				$fileName = substr($match, strrpos($match, '/') + 1);
+				$this->copyFromUrl($fileName, $match);
+			}
+		}
+	}
 
-$doc = new DOMDocument();
-@$doc->loadHTML($html);
-$tags = $doc->getElementsByTagName('img');
-foreach($tags as $t) {
-	$url = $t->getAttribute('src');
-	$id = substr($url, strrpos($url, '/') + 1);
-	$new_img_path = 'img/from-legacy-html/' . $id;
-	copy('http:' . $url, $new_img_path);
-	echo "copied " . $url . " to " . $new_img_path . "<br/>";
+	/*
+	** Copies files from remote urls found in the img tags wihin an html or php file
+	** @param string pathToFile
+	** @return void
+	*/
+
+	public function fromHtml($pathToFile) {
+		$html = file_get_contents($pathToFile);
+		$doc = new DOMDocument();
+		@$doc->loadHTML($html);
+		$tags = $doc->getElementsByTagName('img');
+		foreach($tags as $t) {
+			$remoteUrl = $t->getAttribute('src');
+			$fileName = substr($remoteUrl, strrpos($remoteUrl, '/') + 1);
+			$this->copyFromUrl($fileName, $remoteUrl);
+		}
+	}
+
+	/*
+	** Copies files from a remote URL to a local directory
+	** @param string fileName, string remoteUrl
+	** @return void
+	*/
+
+	private function copyFromUrl($fileName, $remoteUrl) {
+		$newImgPath = $this->imgDir . $fileName;
+		copy($remoteUrl, $newImgPath);
+		echo $remoteUrl . " has been copied to " . $newImgPath . "<br/>";
+	}
+
+	/*
+	** Easily clean the target directory if a mistake has been made
+	** @return void
+	*/
+
+	public function cleanDir() {
+		$files = glob($this->imgDir . '*');
+		foreach($files as $file){
+			if(is_file($file)) {
+				unlink($file);
+				echo "Deleted " . $file . "<br/>";
+			}
+		}
+	}
 }
